ce78ea930a7092d2faf2538e379536958f395083
hbcondo <hbcondo@users.noreply.github.com>
2026-08-31 12:53:30 +0000

    deploy: hbcondo/AmarKota-com@612ed7a28767e96dc8f1cf5774abfa82e4f5e738

---
+++ assets/journal_._tag-B_iCEiRf.js
@@ -0,0 +1,1 @@
+import{s as e}from"./link-BskDqhyN.js";import{r as t}from"./useNavigate-DVBET5Md.js";import{t as n}from"./index--bP-hfu5.js";import{t as r}from"./journal-archive-bMdQiCsz.js";var i=e();function a(){let{tag:e}=n.useParams(),a=Number(e);if(/^\d+$/.test(e)){if(a<2)throw t();return(0,i.jsx)(r,{page:a})}return(0,i.jsx)(r,{selectedTagSlug:e,paginate:!1})}export{a as component};
