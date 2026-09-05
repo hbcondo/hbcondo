f6c716c74ba666248b285437c6fd652952b81624
hbcondo <hbcondo@users.noreply.github.com>
2026-09-05 10:03:26 +0000

    deploy: hbcondo/AmarKota-com@612ed7a28767e96dc8f1cf5774abfa82e4f5e738

--- assets/journal_._tag-YyXrnlL9.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./link-BskDqhyN.js";import{r as t}from"./useNavigate-DVBET5Md.js";import{t as n}from"./index-jx643Rdi.js";import{t as r}from"./journal-archive-D2eoB7sf.js";var i=e();function a(){let{tag:e}=n.useParams(),a=Number(e);if(/^\d+$/.test(e)){if(a<2)throw t();return(0,i.jsx)(r,{page:a})}return(0,i.jsx)(r,{selectedTagSlug:e,paginate:!1})}export{a as component};
