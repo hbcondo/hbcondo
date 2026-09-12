88362839d846eb20432876fc8155ef3044a801ee
hbcondo <hbcondo@users.noreply.github.com>
2026-09-12 10:12:53 +0000

    deploy: hbcondo/AmarKota-com@548b253aa491c0aeee524ede3e44541734e0deef

---
+++ assets/journal_._tag-qyjyUCyG.js
@@ -0,0 +1,1 @@
+import{s as e}from"./link-BskDqhyN.js";import{r as t}from"./useNavigate-DVBET5Md.js";import{t as n}from"./index-DK68l8jr.js";import{t as r}from"./journal-archive-Br_D6OJ6.js";var i=e();function a(){let{tag:e}=n.useParams(),a=Number(e);if(/^\d+$/.test(e)){if(a<2)throw t();return(0,i.jsx)(r,{page:a})}return(0,i.jsx)(r,{selectedTagSlug:e,paginate:!1})}export{a as component};
